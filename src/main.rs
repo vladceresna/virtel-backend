d0584d9ef7d8dd484288a47706ffde8f2c272ad2
Vlad Ĉereŝna <vladceresna6@gmail.com>
2024-08-13 21:34:02 +0200

    Auth set up with db

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,25 @@
 #[macro_use] extern crate rocket;
-mod auth;
+use rocket::{get, post, form::Form, routes};
+use rocket_auth::{Users, Error, Auth, Signup, Login};
+use std::env;
+
+#[post("/signup", data="<form>")]
+async fn signup(form: Form<Signup>, auth: Auth<'_>) -> Result<&'static str, Error> {
+    auth.signup(&form).await?;
+    auth.login(&form.into());
+    Ok("You signed up.")
+}
+
+#[post("/login", data="<form>")]
+async fn login(form: rocket::serde::json::Json<Login>, auth: Auth<'_>) -> Result<&'static str, Error> {
+    auth.login(&form).await?;
+    Ok("You're logged in.")
+}
+
+#[get("/logout")]
+fn logout(auth: Auth<'_>) {
+    auth.logout();
+}
 
 #[get("/")]
 fn index() -> &'static str {
@@ -11,12 +31,13 @@ fn authed(user: User) -> &'static str {
 }
 
 #[tokio::main]
-#[launch]
-async fn rocket() -> Result<(), Error>{
-    let users = Users::open_sqlite("mydb.db").await?;
+async fn main() -> Result<(), Error>{
+    let url = env::var("POSTGRES_URL").expect("Env not setted");
+    let users = Users::open_postgres(url.as_str()).await?;
+
     rocket::build()
-    .mount("/", routes![index, signup, login, logout])
-    .manage(users)
-    .launch();
+        .mount("/", routes![signup, login, logout, index, authed])
+        .manage(users)
+        .launch();
     Ok(())
 }
